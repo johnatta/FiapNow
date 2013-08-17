486ea032c01371ba1ac311b08ffb205844b4cce9
javaxsolutions@gmail.com <javaxsolutions@gmail.com@b91d11d3-e427-2b7c-8c24-3ae04380af71>
2013-08-17 22:39:09 +0000

    grazi

--- ProjetoAM_JavaX/src/br/com/fiap/dao/GrupoDAO.java
+++ ProjetoAM_JavaX/src/br/com/fiap/dao/GrupoDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 import br.com.fiap.entity.Grupo;
 
 public interface GrupoDAO extends DAO<Grupo, Integer> {
-	List<Grupo> getbuscaGruposPorUsuario(Collection<Integer> codPessoa);
+	List<Grupo> buscaGruposPorUsuario(int codPessoa);
 	
 	Grupo buscaInfoBasicas(int codGrupo);
 	
