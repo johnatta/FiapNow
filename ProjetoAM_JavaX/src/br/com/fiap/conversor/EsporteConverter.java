1aa63ef207023086cc0579b0262d617ea3deff7d
javaxsolutions@gmail.com <javaxsolutions@gmail.com@b91d11d3-e427-2b7c-8c24-3ae04380af71>
2013-09-08 00:08:41 +0000

    grazi

--- ProjetoAM_JavaX/src/br/com/fiap/conversor/EsporteConverter.java
+++ ProjetoAM_JavaX/src/br/com/fiap/conversor/EsporteConverter.java
@@ -1,39 +1,25 @@
 package br.com.fiap.conversor;
 
+import java.util.Map;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
 import br.com.fiap.entity.Esporte;
-import br.com.fiap.mb.GrupoBean;
+import br.com.fiap.mb.CriacaoGrupoBean;
 
 @FacesConverter(value="esporteConverter")
 public class EsporteConverter implements Converter {
 
 	//Executado no momento em que a página é submetida.
 	public Object getAsObject(FacesContext fc, UIComponent component, String value) {
-//		if (value.equals("") || value == null) {
-//			return null;
-//		}
-//		Esporte esporte =
-//				(Esporte)fc.getViewRoot().getViewMap().get("grupoBean");
-//		
-//		ClienteTO cliente = new ClienteTO();
-//		for (ClienteTO cli : pedidoBean.getClientes()) {
-//			if(cli.getNmCliente().toLowerCase().equals(value.toLowerCase())) {
-//				cliente = cli;
-//			}
-//		}
-//		return cliente;
-		
-		GrupoBean grupoBean = (GrupoBean)fc.getViewRoot().getViewMap().get("grupoBean");
-		
-		return grupoBean.getEsporte(); 
+		CriacaoGrupoBean criacaoGrupoBean = (CriacaoGrupoBean)fc.getViewRoot().getViewMap().get("criacaoGrupoBean");
+		return criacaoGrupoBean.getEsporte();
 	}
 	//Executado no momento em que a página é renderizada e a cada retorno de uma requisição.
 	public String getAsString(FacesContext fc, UIComponent component, Object value) {
-
 		if (((Esporte) value).getNome() == null || ((Esporte) value).getNome().equals("")) {
 			return "";
 		} else {
