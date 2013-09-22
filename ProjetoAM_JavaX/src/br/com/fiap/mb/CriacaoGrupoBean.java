a3dbd4f14090922d98ca019ffdf4af963a49476e
javaxsolutions@gmail.com <javaxsolutions@gmail.com@b91d11d3-e427-2b7c-8c24-3ae04380af71>
2013-09-22 00:36:05 +0000

    grazi

--- ProjetoAM_JavaX/src/br/com/fiap/mb/CriacaoGrupoBean.java
+++ ProjetoAM_JavaX/src/br/com/fiap/mb/CriacaoGrupoBean.java
@@ -13,6 +13,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.persistence.EntityManager;
 
 import org.apache.commons.io.IOUtils;
@@ -22,14 +23,17 @@ import org.primefaces.event.FlowEvent;
 import br.com.fiap.banco.EntityManagerFactorySingleton;
 import br.com.fiap.dao.EsporteDAO;
 import br.com.fiap.dao.GrupoDAO;
+import br.com.fiap.dao.ModeradorGrupoDAO;
 import br.com.fiap.dao.PessoaDAO;
 import br.com.fiap.daoimpl.EsporteDAOImpl;
 import br.com.fiap.daoimpl.GrupoDAOImpl;
+import br.com.fiap.daoimpl.ModeradorGrupoDAOImpl;
 import br.com.fiap.daoimpl.PessoaDAOImpl;
 import br.com.fiap.datamodel.EsporteDataModel;
 import br.com.fiap.datamodel.PessoaDataModel;
 import br.com.fiap.entity.Esporte;
 import br.com.fiap.entity.Grupo;
+import br.com.fiap.entity.ModeradorGrupo;
 import br.com.fiap.entity.Pessoa;
 import br.com.fiap.entity.Privacidade;
 
@@ -40,17 +44,17 @@ public class CriacaoGrupoBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private List<Privacidade> privs;
 	private Esporte[] espSelecionados;
-	private Pessoa[] pesSelecionados;
+	private Pessoa[] membrosSelecionados;
+	private Pessoa[] moderadorSelecionados;
 	private Grupo grupo;
-	private List<Grupo> grupos ;
 	private Pessoa pessoa;
 	private List<Pessoa> pessoas;
 	private List<Esporte> esportes;
 	private EsporteDataModel edm;
 	private PessoaDataModel pdm;
+	private PessoaDataModel mdm;
 	private List<Esporte> listEsporte;
-	private static Logger logger = Logger.getLogger(CriacaoGrupoBean.class.getName());
-	private boolean skip; 
+	private ModeradorGrupo moderadorGrupo;
 	
 	@PostConstruct
 	public void init(){
@@ -58,57 +62,102 @@ public class CriacaoGrupoBean implements Serializable {
 		EsporteDAO espDAO = new EsporteDAOImpl(em);
 		listEsporte = new ArrayList<Esporte>();
 		grupo = new Grupo();
-
+		moderadorGrupo = new ModeradorGrupo(); 
+		
 		pessoas = pesDAO.buscarTodasPessoas();
 		esportes = espDAO.buscarTodosEsportes();		
-		
+
 		this.privs = Arrays.asList(grupo.getPrivacidade().values());
 		edm = new EsporteDataModel(esportes); 
 		pdm = new PessoaDataModel(pessoas);
-		
+		mdm = new PessoaDataModel(pessoas);
+
 		FacesContext context = FacesContext.getCurrentInstance();
 		Map<String, Object> map = context.getExternalContext().getSessionMap();
 		LoginBean sessao = (LoginBean)map.get("loginBean");
 		pessoa = sessao.getPessoa();
 	}
 
-	public String btnCriarGrupo(){
-		String retorno;
+	public void informacaoBasicaGrupo(ActionEvent event) {  
+		PessoaDAO pDAO = new PessoaDAOImpl(em);
+		FacesContext fc = FacesContext.getCurrentInstance();
+		
+		if(getGrupo().getDescricao() != null || getGrupo().getPrivacidade() != null 
+				|| getGrupo().getNomeGrupo() != null || getEspSelecionados() != null){
+
+			grupo.setAdm(pDAO.buscarInformacoes(pessoa.getCodPessoa()));
+			for (Esporte esporte : getEspSelecionados()) {
+				listEsporte.add(esporte);
+			}
+			grupo.setEsportes(listEsporte);
+			
+			FacesMessage fm = new FacesMessage();
+			fm.setSummary("Informações grupo salvas, faça o próximo passo");
+			fc.addMessage("", fm);
+		}else{
+			FacesMessage fm = new FacesMessage("Campo obrigatório não preenchido. Favor preencher.");
+			fm.setSeverity(FacesMessage.SEVERITY_ERROR);
+			fc.addMessage("messages", fm);
+		}
+	}
+
+	public String passarParaAddPessoa(){
+		return "adicionar_membro_grupo";  
+	}
+
+	public void relacionarMembros(ActionEvent event) {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		if(getMembrosSelecionados().length != 0){
+			FacesMessage fm = new FacesMessage();
+			fm.setSummary("Membros relacionados, faça o próximo passo.");
+			fc.addMessage("", fm);
+		}
+	} 
+
+	public String passarParaAddModerador(){
+		return "adicionar_moderador_grupo";  
+	}
+
+	public void relacionarModerador(ActionEvent event) {  
+		FacesContext fc = FacesContext.getCurrentInstance();
+		if(getMembrosSelecionados().length != 0){
+			FacesMessage fm = new FacesMessage();
+			fm.setSummary("Membros relacionados, faça o próximo passo.");
+			fc.addMessage("", fm);
+		}
+	} 
+
+	public String concluirCriacaoGrupo() {  
 		PessoaDAO pDAO = new PessoaDAOImpl(em);
 		GrupoDAO gDAO = new GrupoDAOImpl(em);
-		grupos = new ArrayList<Grupo>();
+		ModeradorGrupoDAO moderadorDAO = new ModeradorGrupoDAOImpl(em);
+		
 		FacesContext fc = FacesContext.getCurrentInstance();
 		FacesMessage fm = new FacesMessage();
-		
-		grupo.setAdm(pDAO.buscarInformacoes(pessoa.getCodPessoa()));
-		//Arrays.asList(getEspSelecionados())
-		for (Esporte esporte : getEspSelecionados()) {
-			listEsporte.add(esporte);
-		}
-		
-		
-		grupo.setEsportes(listEsporte);
 		try{
 			grupo = gDAO.insertEntity(grupo);
-			for (Pessoa p : getPesSelecionados()){
-				p.getGruposParticipantes().add(grupo);
-			}
-			//pessoa.getGruposParticipantes().add(grupo);
-			
-			//pessoa.getGruposModerado().add(grupo);
-			//grupos.add(grupo);
-			//pessoa.setGrupos(grupos);
+			pessoa.getGruposParticipantes().add(grupo);
 			pDAO.update(pessoa);
+			
+			for (Pessoa membro : getMembrosSelecionados()){
+				membro.getGruposParticipantes().add(grupo);
+				pDAO.update(membro);
+			}   
+			for (Pessoa moderador : getModeradorSelecionados()){
+				moderadorGrupo.setGrupo(grupo);
+				moderadorGrupo.setPessoa(moderador);
+				moderadorDAO.insert(moderadorGrupo);
+			}
+			
 			fm.setSummary("Cadastro Realizado com Sucesso");
 			fc.addMessage("", fm);
-			retorno = "grupo";
+			return "grupo";
 		} catch(Exception e){
 			e.printStackTrace();
 			fm.setSummary("Erro na Realização do Cadastro");
 			fc.addMessage("", fm);
-			retorno = "";
+			return "";
 		}
-		return retorno;
 	}
 
 	public void realizarUpload(FileUploadEvent event) {
@@ -126,25 +175,36 @@ public class CriacaoGrupoBean implements Serializable {
 		}
 	}
 
-	 public String onFlowProcess(FlowEvent event) {  
-		 //	String currentStepId = event.getOldStep();
-		 	String stepToGo = event.getNewStep();
-	          
-	        if(skip) {  
-	            skip = false;   //reset in case user goes back  
-	            return "moderador";  
-	        }  
-	        else {  
-	            return event.getNewStep();  
-	        }  
-	    }  
-	
 	public List<Privacidade> getPrivs() {
 		return privs;
 	}
 
 	public void setPrivs(List<Privacidade> privs) {
 		this.privs = privs;
+	}
+
+	public Esporte[] getEspSelecionados() {
+		return espSelecionados;
+	}
+
+	public void setEspSelecionados(Esporte[] espSelecionados) {
+		this.espSelecionados = espSelecionados;
+	}
+
+	public Pessoa[] getMembrosSelecionados() {
+		return membrosSelecionados;
+	}
+
+	public void setMembrosSelecionados(Pessoa[] membrosSelecionados) {
+		this.membrosSelecionados = membrosSelecionados;
+	}
+
+	public Pessoa[] getModeradorSelecionados() {
+		return moderadorSelecionados;
+	}
+
+	public void setModeradorSelecionados(Pessoa[] moderadorSelecionados) {
+		this.moderadorSelecionados = moderadorSelecionados;
 	}
 
 	public Grupo getGrupo() {
@@ -163,6 +223,14 @@ public class CriacaoGrupoBean implements Serializable {
 		this.pessoa = pessoa;
 	}
 
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
 	public List<Esporte> getEsportes() {
 		return esportes;
 	}
@@ -179,20 +247,20 @@ public class CriacaoGrupoBean implements Serializable {
 		this.edm = edm;
 	}
 
-	public Esporte[] getEspSelecionados() {
-		return espSelecionados;
+	public PessoaDataModel getPdm() {
+		return pdm;
 	}
 
-	public void setEspSelecionados(Esporte[] espSelecionados) {
-		this.espSelecionados = espSelecionados;
+	public void setPdm(PessoaDataModel pdm) {
+		this.pdm = pdm;
 	}
 
-	public List<Grupo> getGrupos() {
-		return grupos;
+	public PessoaDataModel getMdm() {
+		return mdm;
 	}
 
-	public void setGrupos(List<Grupo> grupos) {
-		this.grupos = grupos;
+	public void setMdm(PessoaDataModel mdm) {
+		this.mdm = mdm;
 	}
 
 	public List<Esporte> getListEsporte() {
@@ -203,37 +271,13 @@ public class CriacaoGrupoBean implements Serializable {
 		this.listEsporte = listEsporte;
 	}
 
-	public boolean isSkip() {
-		return skip;
+	public ModeradorGrupo getModeradorGrupo() {
+		return moderadorGrupo;
 	}
 
-	public void setSkip(boolean skip) {
-		this.skip = skip;
+	public void setModeradorGrupo(ModeradorGrupo moderadorGrupo) {
+		this.moderadorGrupo = moderadorGrupo;
 	}
 
-	public List<Pessoa> getPessoas() {
-		return pessoas;
-	}
-
-	public void setPessoas(List<Pessoa> pessoas) {
-		this.pessoas = pessoas;
-	}
-
-	public Pessoa[] getPesSelecionados() {
-		return pesSelecionados;
-	}
-
-	public void setPesSelecionados(Pessoa[] pesSelecionados) {
-		this.pesSelecionados = pesSelecionados;
-	}
-
-	public PessoaDataModel getPdm() {
-		return pdm;
-	}
-
-	public void setPdm(PessoaDataModel pdm) {
-		this.pdm = pdm;
-	}
-
-
+	
 }
