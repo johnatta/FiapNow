2001e3264a6a4b13f2c657c201676e29927b185f
javaxsolutions@gmail.com <javaxsolutions@gmail.com@b91d11d3-e427-2b7c-8c24-3ae04380af71>
2013-09-28 23:22:22 +0000

    grazi

--- ProjetoAM_JavaX/src/br/com/fiap/teste/TesteGrazi.java
+++ ProjetoAM_JavaX/src/br/com/fiap/teste/TesteGrazi.java
@@ -39,13 +39,13 @@ public class TesteGrazi {
 		/*
 		Pessoa pes = new Pessoa();
 		PessoaDAO pesDAO = new PessoaDAOImpl(em);
-		
+
 		pes = pesDAO.buscarInformacoes(2);
-		
+
 		System.out.println("Nome: " + pes.getNome());
 		System.out.println("Cidade: " + pes.getCodEndereco().getCidade());
 		System.out.println("Tel: " + pes.getTelRes());
-		*/
+		 */
 		List<Grupo> grupos = new ArrayList<Grupo>();
 		Grupo g = new Grupo();
 		List<Esporte> esportes = new ArrayList<Esporte>();
@@ -59,37 +59,38 @@ public class TesteGrazi {
 		ModeradorGrupo modGrupo = new ModeradorGrupo();
 		ModeradorGrupoDAO modDAO = new ModeradorGrupoDAOImpl(em);
 		List<Pessoa> moderadores = modDAO.buscarModeradoresDoGrupoRowNum(6);
-		
+		GrupoDAO gDAO = new GrupoDAOImpl(em); 
+
+		/*
 		for (Pessoa mod : moderadores) {
 			//System.err.println(mod.getNome());
 		}
-		
-		GrupoDAO gDAO = new GrupoDAOImpl(em); 
+
 		List<ComentarioGrupoRC> comentarios = gDAO.buscarComentariosPeloGrupo(4);
 		System.out.println(comentarios);
-		
+
 		for(ComentarioGrupoRC comentarioGrupo : comentarios){
 			System.err.println("APELIDO: " + comentarioGrupo.getApelido());
 			System.err.println("COMENTARIO: " + comentarioGrupo.getComentario());
 			System.err.println("HORA: " + comentarioGrupo.getDataHora());
-		
+
 			//Calendar dataC = comentarioGrupo.getDataHora().getInstance();
 			//Date data = dataC.getTime();
 			//Date data = comentarioGrupo.getDataHora().getInstance().getTime();
 			//System.out.println(data.getTime());
-			
+
 			Date data = comentarioGrupo.getDataHora().getTime();
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm"); 
 			System.out.println(sdf.format(data));
 			//String dataFormatada = sdf.format(data);
 			//System.out.println(dataFormatada);
 			/*
-			 * Calendar c = Calendar.getInstance(); Date data = c.getTime(); DateFormat f = DateFormat.getDateInstance(); Date data2 = f.parse("12/01/1995"); System.out.println(data2); SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); System.out.println("Data formatada: "+sdf.format(data)); //Converte Objetos System.out.println(“Data convertida: ”+sdf.parse("02/08/1970")); }
+		 * Calendar c = Calendar.getInstance(); Date data = c.getTime(); DateFormat f = DateFormat.getDateInstance(); Date data2 = f.parse("12/01/1995"); System.out.println(data2); SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); System.out.println("Data formatada: "+sdf.format(data)); //Converte Objetos System.out.println(“Data convertida: ”+sdf.parse("02/08/1970")); }
 
 Leia mais em: Trabalhando com as classes Date, Calendar e SimpleDateFormat em Java http://www.devmedia.com.br/trabalhando-com-as-classes-date-calendar-e-simpledateformat-em-java/27401#ixzz2fgvoEtov
-			 * 
-			 * 
-			 * 
+		 * 
+		 * 
+		 * 
 		Calendar c = Calendar.getInstance();
 		Date data = c.getTime(); 
 		DateFormat f = DateFormat.getDateInstance(); 
@@ -98,21 +99,23 @@ Leia mais em: Trabalhando com as classes Date, Calendar e SimpleDateFormat em Ja
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
 		System.out.println("Data formatada: "+sdf.format(data));
 		//Converte Objetos System.out.println(“Data convertida: ”+sdf.parse("02/08/1970")); }
-*/
 		}
-		
-		
-		
-		/*
-		g = gDAO.buscaInfoBasicas(2);
+
+		System.err.println("---------------------------------------------------------------------");
+
+		g = gDAO.buscarInfoGrupo(2);
 		System.out.println("Nome: " + g.getNomeGrupo() );
 		System.out.println("Esporte: " + g.getEsportes().set(1, e).getNome());
 		System.out.println("Privacidade: " + g.getPrivacidade());
 		System.out.println("Quantidade: " + g.getQuantidade());
 
-		grupos = gDAO.buscaGruposDoUsuario(2);
+		System.err.println("---------------------------------------------------------------------");
+
+		p = pDAO.buscarInformacoes(1);
+
+		grupos = gDAO.buscaGruposDaPessoa(p);
 		for (Grupo grupo : grupos) {
-			System.out.println("CODIGO GRUPO " + grupo.getCodGrupo());
+			System.err.println("CODIGO GRUPO " + grupo.getCodGrupo());
 			System.out.println("DESCRICAO " + grupo.getDescricao());
 			System.out.println("NOME " + grupo.getNomeGrupo());
 			for(Esporte esp : grupo.getEsportes() ){
@@ -121,8 +124,11 @@ Leia mais em: Trabalhando com as classes Date, Calendar e SimpleDateFormat em Ja
 			System.out.println("PRIVACIDADE " + grupo.getPrivacidade());
 			System.out.println("IMAGEM " + grupo.getImgGrupo());
 			System.out.println();
+			System.err.println("---------------------------------------------------------------------");
 		}
-		
+		 */
+		/*
+
 		grupos = gDAO.buscaGruposPorUsuario(2);
 		for (Grupo grupo : grupos) {
 			System.out.println("CODIGO GRUPO " + grupo.getCodGrupo());
@@ -133,7 +139,7 @@ Leia mais em: Trabalhando com as classes Date, Calendar e SimpleDateFormat em Ja
 			System.out.println("Quantidade " + grupo.getQuantidade());
 			System.out.println();
 		}
-		
+
 		esportes = espDAO.buscarTodosEsportes();
 		for (Esporte esporte : esportes) {
 			System.out.println(esporte.getCodEsporte());
@@ -142,15 +148,26 @@ Leia mais em: Trabalhando com as classes Date, Calendar e SimpleDateFormat em Ja
 		g = gDAO.buscarGrupoCadastrado(2);
 		System.out.println("NOME : " + g.getNomeGrupo());
 		System.out.println("CODIGO : " + g.getCodGrupo());
-		 */
-		
-		
+
+
 		//numMembros = gDAO.buscarNumeroMembros(56);
 		//System.out.println(numMembros);
 		//e = espDAO.buscarPorNome("corrida");
 		//System.out.println(e.getNome());
-		
-		
+
+
+		grupos = gDAO.buscarGrupos();
+
+		for (Grupo grupoA : grupos) {
+			System.out.println(grupoA.getDescricao());
+		}
+
+		grupos = gDAO.consultaMeusGrupos(1);
+
+		for (Grupo grupoA : grupos) {
+			System.out.println(grupoA.getDescricao());
+		}
+		 */
 	}
 
 }
