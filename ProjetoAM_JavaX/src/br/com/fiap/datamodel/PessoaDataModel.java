b1756ae10b80807e3ec9ce26423642a87006d136
javaxsolutions@gmail.com <javaxsolutions@gmail.com@b91d11d3-e427-2b7c-8c24-3ae04380af71>
2013-10-06 02:25:34 +0000

    grazi

--- ProjetoAM_JavaX/src/br/com/fiap/datamodel/PessoaDataModel.java
+++ ProjetoAM_JavaX/src/br/com/fiap/datamodel/PessoaDataModel.java
@@ -10,13 +10,28 @@ import br.com.fiap.entity.Pessoa;
 
 public class PessoaDataModel extends ListDataModel<Pessoa> implements SelectableDataModel<Pessoa> {
 
+	/**
+	 * Construtor padrão
+	 * @author Graziele Vasconcelos
+	 */
+	public PessoaDataModel(){	
+	}
+	
+	/**
+	 * Método construtor que recebe uma lista de pessoas(objeto que será selecionado)
+	 * @param lista de pessoas 
+	 * @author Graziele Vasconcelos
+	 */		
 	public PessoaDataModel(List<Pessoa> pessoas) {
 		super(pessoas);
 	}
-	
-	public PessoaDataModel(){
-		
-	}
+
+	/**
+	 * Realiza a identificação da seleção do usuário
+	 * @param código da pessoa selecionada
+	 * @return lista de pessoas selecionadas ou nulo caso não encontre na lista
+	 * @author Graziele Vasconcelos
+	 */
 	@Override
 	public Pessoa getRowData(String rowKey) {
 		List<Pessoa> pes = (List<Pessoa>) getWrappedData(); 
@@ -28,6 +43,13 @@ public class PessoaDataModel extends ListDataModel<Pessoa> implements Selectable
 		return null;
 	}
 
+	/**
+	 * Recebe o objeto Pessoa para converter para apenas os códigos do mesmo
+	 * para que quando o usuário selecione a aplicação possa identificar posteriormente
+	 * @param instância do objeto Pessoa
+	 * @return o código da pessoa para ser identificado posteriormente e para o selector efetuar seu processo 
+	 * @author Graziele Vasconcelos
+	 */
 	@Override
 	public Object getRowKey(Pessoa p) {
 		return p.getCodPessoa();
