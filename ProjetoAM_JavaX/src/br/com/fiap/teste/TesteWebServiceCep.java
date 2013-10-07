78e4adae9746c07447a68615ecb3c9bbd26f19cf
javaxsolutions@gmail.com <javaxsolutions@gmail.com@b91d11d3-e427-2b7c-8c24-3ae04380af71>
2013-10-07 23:38:09 +0000

    AHM

---
+++ ProjetoAM_JavaX/src/br/com/fiap/teste/TesteWebServiceCep.java
@@ -0,0 +1,34 @@
+package br.com.fiap.teste;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class TesteWebServiceCep {
+
+	public static void main(String[] args){
+
+		try {
+			JAXBContext jc = JAXBContext.newInstance(Webservicecep.class);
+
+			Unmarshaller u = jc.createUnmarshaller();
+			URL url = new URL( "http://cep.republicavirtual.com.br/web_cep.php?cep=09371160&formato=xml" );
+			Webservicecep cep = (Webservicecep) u.unmarshal( url );
+
+			System.out.println(cep.getCidade());
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
